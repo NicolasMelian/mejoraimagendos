254cd62ea60b614d2b5ec4998d8cdb77352f0cc0
Nico Melian <nicolasgmelian@gmail.com>
2024-03-05 18:41:36 -0300

    asd

--- app/Listeners/UpdateSubscriptionCredits.php
+++ app/Listeners/UpdateSubscriptionCredits.php
@@ -2,50 +2,44 @@
 
 namespace App\Listeners;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
-use Spark\Events\SubscriptionUpdated;
+use Laravel\Paddle\Events\SubscriptionUpdated;
 use App\Models\User;
-use Illuminate\Support\Facades\Log;
 
 
 class UpdateSubscriptionCredits
 {
     /**
-     * Create the event listener.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Handle the event.
      */
     public function handle(SubscriptionUpdated $event): void
     {
-        \Log::info(print_r($event, true));
 
-        // log evento
-        \Log::info('Evento SubscriptionUpdated');
+// Asumiendo que $event->payload ya es un array asociativo como se muestra en tu ejemplo
+$priceId = $event->payload['data']['items'][0]['price']['id'];
 
+// Encuentra al usuario basado en el customer_id
+$customerId = $event->payload['data']['customer_id'];
+$user = User::where('customer_id', $customerId)->first();
 
+if (!$user) {
+    Log::error("Usuario no encontrado con customer_id: {$customerId}");
+    return;
+}
 
+// Asigna créditos basado en el price_id
+switch ($priceId) {
+    case 'pri_01ha2h3cqg5fervw0zr2zehk0b': // ID para el plan anual
+        $user->credits += 10000;
+        break;
+    case 'pri_01ha2h29b39sgwd9rj5ebwn7jr': // ID para el plan mensual
+        $user->credits += 1000;
+        break;
+    default:
+        Log::info("Plan no reconocido con price_id: {$priceId}");
+        return;
+}
 
-        $user = $event->user;
-
-        // log $user 
-        
-        $plan = $event->plan;
-
-        if ($plan->monthly_id) {
-            // Lógica para plan mensual, por ejemplo, reajustar a 1000 créditos
-            $user->credits = 1000;
-        } elseif ($plan->yearly_id) {
-            // Lógica para plan anual, por ejemplo, ajustar a 12000 créditos
-            $user->credits = 12000;
-        }
-
-        $user->save();
+$user->save();
+Log::info("Créditos actualizados para el usuario: {$user->id}, nuevo saldo de créditos: {$user->credits}");
     }
 }
