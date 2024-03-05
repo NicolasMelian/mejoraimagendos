20cde1776ebb5e43412a56ca5ef65f4e304854d3
Nico Melian <nicolasgmelian@gmail.com>
2024-03-05 14:02:36 -0300

    asd

--- config/spark.php
+++ config/spark.php
@@ -93,16 +93,14 @@ return [
         'user' => [
             'model' => User::class,
 
-            'trial_days' => 0,
-
             'default_interval' => 'monthly',
 
             'plans' => [
                 [
                     'name' => 'Mejoraimagen',
                     'short_description' => 'This is a short, human friendly description of the plan.',
-                    'monthly_id' =>  'pri_01ha2h29b39sgwd9rj5ebwn7jr',
-                    'yearly_id' => 'pri_01ha2h3cqg5fervw0zr2zehk0b',
+                    'monthly_id' =>  'pri_01ha2h3cqg5fervw0zr2zehk0b',
+                    'yearly_id' => 'pri_01ha2h29b39sgwd9rj5ebwn7jr',
                     'features' => [
                         'Feature 1',
                         'Feature 2',
