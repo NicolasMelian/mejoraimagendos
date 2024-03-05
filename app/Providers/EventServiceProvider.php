3aa847d121dc912121152090d79fd8fdbd3e7082
Nico Melian <nicolasgmelian@gmail.com>
2024-03-05 17:54:02 -0300

    asd

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-use Laravel\Spark\Events\SubscriptionCreated;
+use Laravel\Paddle\Events\SubscriptionCreated;
 use App\Listeners\UpdateSubscriptionCredits;
 
 class EventServiceProvider extends ServiceProvider
@@ -20,7 +20,7 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        \Laravel\Spark\Events\SubscriptionCreated::class => [
+        Laravel\Paddle\Events\SubscriptionCreated::class => [
             \App\Listeners\UpdateSubscriptionCredits::class,
         ],
 
