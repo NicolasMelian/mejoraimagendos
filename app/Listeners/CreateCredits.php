3bc9706b80f0f826d8e90254214f2a7083167e96
Nico Melian <nicolasgmelian@gmail.com>
2024-03-06 01:09:51 -0300

    asd

--- app/Listeners/CreateCredits.php
+++ app/Listeners/CreateCredits.php
@@ -18,6 +18,8 @@ class CreateCredits
 
         $user = $event->billable;
 
+        
+
         if($event->payload['data']['items'][0]['price']['id'] == 'pri_01ha2h29b39sgwd9rj5ebwn7jr'){
             $credits = 1000;
         }else if($event->payload['data']['items'][0]['price']['id'] == 'pri_01ha2h3cqg5fervw0zr2zehk0b'){
--- app/Listeners/RefreshCredits.php
+++ app/Listeners/RefreshCredits.php
@@ -2,10 +2,10 @@
 
 namespace App\Listeners;
 
+use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Laravel\Paddle\Events\SubscriptionUpdated;
-use App\Models\User;
 
 class RefreshCredits
 {
@@ -17,16 +17,19 @@ class RefreshCredits
     {
         $user = $event->billable;
 
+        $creditsToAdd = 0;
 
-        if($event->payload['data']['items'][0]['price']['id'] == 'pri_01ha2h29b39sgwd9rj5ebwn7jr'){
-            $credits = 1000;
-        } else if ($event->payload['data']['items'][0]['price']['id'] == 'pri_01ha2h3cqg5fervw0zr2zehk0b'){
-            $credits = 10000;
+        $price_id = $event->payload['data']['items'][0]['price']['id'];
+
+        if($price_id == 'pri_01ha2h29b39sgwd9rj5ebwn7jr'){
+            $creditsToAdd = 1000;
+        }else if($price_id == 'pri_01ha2h3cqg5fervw0zr2zehk0b'){
+            $creditsToAdd = 12000;
         } else {
-            $credits = 0;
+            $creditsToAdd = 0;
         }
-
-        $user->credits += $credits;
+    
+        $user->credits = $creditsToAdd;
         $user->save();
 
 
